ae71e4a36b8f83a6db88ffb44b8e23da7939151f
Firangiz <firanhizmamiedova@gmail.com>
2023-09-07 22:13:58 +0400

    commit 2

--- js/data.js
+++ js/data.js
@@ -3,16 +3,19 @@
 let news = [
     {
         newsTitle: 'Bakı metrosunda “BakıKart” ləğv edilir?', 
+        newsImage: '../img/metro.jpg',
         newsContent: 'Yeraltı nəqliyyatdan istifadə edən sərnişinlər metroda “BakıKart”ın ləğv ediləcəyindən narahatdırlar. Ötən gün “Bakı Metropoliteni” QSC-nin iki stansiyada pilot layihənin tətbiqinə başlaması sərnişinlər arasında anlaşılmazlığa səbəb olub.'
     },
 
     {
         newsTitle: 'Azərbaycan musiqisi və rəqsi Çində beynəlxalq sərgidə təqdim edilib', 
+        newsImage: '../img/az-mus.jpg',
         newsContent: 'Azərbaycan musiqisi və rəqsi Çində “VI İpək Yolu Beynəlxalq Mədəniyyət Sərgisi”nin qala-konsert proqramında təqdim edilib. Bu barədə Mədəniyyət Nazilriyindən məlumat verilib.'
     },
 
     {
         newsTitle: 'Kursantlarla praktiki məşğələlər keçirilib', 
+        newsImage: '../img/kursant.jpg',
         newsContent: '2023-cü ilin tədris planı və proqramına uyğun olaraq Heydər Əliyev adına Hərbi İnstitutun kursantları ilə praktiki məşğələlər keçirilib. Əvvəlcə kursantlar simulyasiya trenajorunda dəqiq nişanalma qaydalarını məşq ediblər.'
     }
 ]
@@ -20,9 +23,18 @@ let news = [
 
 
 document.querySelector('.news-title1').innerHTML = news[0].newsTitle;
+document.querySelector('.news-img1').src = news[0].newsImage;
 document.querySelector('.news-content1').innerHTML = news[0].newsContent;
 
 
+document.querySelector('.news-title2').innerHTML = news[1].newsTitle;
+document.querySelector('.news-img2').src = news[1].newsImage;
+document.querySelector('.news-content2').innerHTML = news[1].newsContent;
+
+
+document.querySelector('.news-title3').innerHTML = news[2].newsTitle;
+document.querySelector('.news-img3').src = news[2].newsImage;
+document.querySelector('.news-content3').innerHTML = news[2].newsContent;
 
 
 
--- js/script.js
+++ js/script.js
@@ -1,6 +1,8 @@
 let btn = document.querySelector('.btn');
 let boxes = document.querySelector('.boxes');
-let change_news = document.querySelector('.news-btn1');
+let change_news1 = document.querySelector('.news-btn1');
+let change_news2 = document.querySelector('.news-btn2');
+let change_news3 = document.querySelector('.news-btn3');
 
 btn.addEventListener('click', hideShow);
 function hideShow() {
@@ -9,16 +11,34 @@ function hideShow() {
     
 }
 
+change_news1.addEventListener('click', changeNews1);
+change_news2.addEventListener('click', changeNews2);
+change_news3.addEventListener('click', changeNews3);
 
-
-
-change_news.addEventListener('click', changeNews);
-function changeNews() {
-
+function changeNews1() {
 let randomNews = Math.floor(Math.random()  * news.length);
 document.querySelector('.news-title1').innerHTML = news[randomNews].newsTitle;
+document.querySelector('.news-img1').src = news[randomNews].newsImage;
 document.querySelector('.news-content1').innerHTML = news[randomNews].newsContent;
 }
+
+
+function changeNews2() {
+    let randomNews = Math.floor(Math.random()  * news.length);
+    document.querySelector('.news-title2').innerHTML = news[randomNews].newsTitle;
+    document.querySelector('.news-img2').src = news[randomNews].newsImage;
+    document.querySelector('.news-content2').innerHTML = news[randomNews].newsContent;
+}
+
+
+function changeNews3() {
+        let randomNews = Math.floor(Math.random()  * news.length);
+        document.querySelector('.news-title3').innerHTML = news[randomNews].newsTitle;
+        document.querySelector('.news-img3').src = news[randomNews].newsImage;
+        document.querySelector('.news-content3').innerHTML = news[randomNews].newsContent;
+ }
+
+
 
 
 
